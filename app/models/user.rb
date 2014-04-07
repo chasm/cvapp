a39c32112b00cf1fff178a623d0a565659b600cb
chasm <charles.munat@gmail.com>
2014-04-07 17:12:34 +0800

    Added authenticate methods to user. New error: undefined method 'fish' for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
 
   attr_accessor :password, :password_confirmation
@@ -7,5 +9,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, if: :password
 
   def self.authenticate(email, password)
+    user = User.find_by email: email
+    user if user and user.authenticate(password)
+  end
+
+  def authenticate(password)
+    self.fish == BCrypt::Engine.hash_secret(password, self.salt)
   end
 end
