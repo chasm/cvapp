14693bf5ee70660712ab0e0d27069b89f60c74a3
chasm <charles.munat@gmail.com>
2014-04-07 13:47:02 +0800

    Added model spec for password confirmation. New error: undefined method 'password='.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,17 @@ describe User do
       expect(u).to be_valid
     end
   end
+
+  describe "passwords" do
+    context "when setting the password" do
+      it "needs a password and a confirmation" do
+        u = User.create( email: "bob@example" )
+        p = "123"
+
+        u.password = p
+        u.save
+        expect(u).not_to be_valid
+      end
+    end
+  end
 end
