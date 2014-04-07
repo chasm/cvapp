d3a1139365385f6ba471298d8f8c7d28fc3bb071
chasm <charles.munat@gmail.com>
2014-04-07 14:10:51 +0800

    Added password confirmation validation. Sign in error remains.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   attr_accessor :password, :password_confirmation
 
   validates :email, presence: true
+  validates :password, confirmation: true
   validates :password_confirmation, presence: true, if: :password
 end
